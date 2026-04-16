de7d413aa05314b846ef5d898166c9865e448162
Pablo <alu0101546001@ull.edu.es>
2026-04-16 16:34:45 +0100

    return true or false

---
+++ returns_one.js
@@ -0,0 +1,18 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas 2025-2026
+ *
+ * @since April 16 2026
+ * @desc Always returns true
+ */
+
+
+
+/**
+ * Main function to demonstrate the use of returnsTrue
+ */
+function mainFunction() {
+    return 1;
+}
