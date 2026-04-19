71c1a730bf13e2654d8afd778e8359746bd2aa62
IvanLuisEst <alu0101543581@ull.edu.es>
2026-04-19 15:31:18 +0100

    Changes vite-fundamentals

--- 25-26-pai-p10-mvc-events-projectile-IvanLuisEst-2-main/src/exercises/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './home-work/parabolic-trajectory/model/projectile-model';
-export * from './home-work/parabolic-trajectory/view/graph/graph-config';
-export * from './home-work/parabolic-trajectory/view/graph/graph';
-export * from './home-work/parabolic-trajectory/view/information-view';
-export * from './home-work/parabolic-trajectory/view/controls-view';
-export * from './home-work/parabolic-trajectory/view/projectile-view';
-export * from './home-work/parabolic-trajectory/controller/proyectile-controller';
